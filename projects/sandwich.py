6c6ab342252a0913ff8b35effa05ca2931dedc21
pelB <pelayob@saintjamesacademy.com>
2025-02-14 20:12:57 +0000

    sandwich thingy

---
+++ projects/sandwich.py
@@ -0,0 +1,7 @@
+"""
+ingredients = []
+robot = ("diddyspinkytoe")
+sandwitch = ingredientsinorder
+currentingredient = input(put in one ingredient. make sure your ingredients are in order!)
+ingredientsinorder = 
+"""
